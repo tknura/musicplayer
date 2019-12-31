d15a88dd91e6201fc56e55c4151f176176a77678
Tomasz Knura <tknura@gmail.com>
2019-12-31 15:44:25 +0100

    Added empty classes and fixes

---
+++ musicplayer/src/com/pl/wylegly/musicManager/Album.java
@@ -0,0 +1,8 @@
+package com.pl.wylegly.musicManager;
+
+public class Album {
+
+	public Album() {
+		
+	}
+}
--- musicplayer/src/com/pl/wylegly/musicManager/Song.java
+++ musicplayer/src/com/pl/wylegly/musicManager/Song.java
@@ -2,6 +2,8 @@ package com.pl.wylegly.musicManager;
 
 public class Song {
 	
+	private Details songDetails;
+	
 	public Song() {
 		// TODO Auto-generated constructor stub
 	}
