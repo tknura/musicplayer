4d5aa9409627a17de8b06b03d996aed41cde5604
Tomasz Knura <tknura@gmail.com>
2020-01-02 21:58:42 +0100

    Addded sorting methods in playlist

--- musicplayer/src/com/pl/musicManager/Playlist.java
+++ musicplayer/src/com/pl/musicManager/Playlist.java
@@ -1,5 +1,31 @@
 package com.pl.musicManager;
 
-public class Playlist extends SongList{
+import java.util.Comparator;
 
+public class Playlist extends SongList{
+	
+	private String name;
+
+	public Playlist(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void sortByAlbums() {
+		Comparator<Song> compareByAlbum = (Song s1, Song s2) -> s1.getAlbumName().compareTo(s2.getAlbumName());
+		songs.sort(compareByAlbum);
+	}
+	
+	public void sortByArists() {
+		Comparator<Song> compareByArtists = (Song s1, Song s2) -> s1.getSongDetails().getArtist().compareTo(s2.getSongDetails().getArtist());
+		this.songs.sort(compareByArtists);
+	}
 }
