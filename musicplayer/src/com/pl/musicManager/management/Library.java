7df6c045209bee006af1c5a2eec845e2545c7098
Błażej Wylęgły <blazej.wylegly@gmail.com>
2020-01-10 17:48:27 +0100

    Added JSON jars, basics of library creation

--- musicplayer/src/com/pl/musicManager/management/Library.java
+++ musicplayer/src/com/pl/musicManager/management/Library.java
@@ -1,10 +1,23 @@
 package com.pl.musicManager.management;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
 import java.util.LinkedList;
 
+import org.hildan.fxgson.FxGson;
+import org.hildan.fxgson.FxGson.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder.*;
 import com.pl.musicManager.Album;
 import com.pl.musicManager.Artist;
 import com.pl.musicManager.Playlist;
+import com.pl.musicManager.Song;
 import com.pl.musicManager.SongList;
   
 public class Library {
@@ -29,5 +42,22 @@ public class Library {
 	
 	public void retrieveSongList() {
 		this.songList = new SongList();
+	}
+	
+	public void writeToJSON()  {
+		try {
+			Path source = Paths.get("src/resources");
+			FileWriter fileWriter = new FileWriter(source.toAbsolutePath().toString() + "/library.json");
+			System.out.println(source.toAbsolutePath().toString());
+			
+			Gson gson = FxGson.coreBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+			
+			Song song = new Song("Sunflower", "Post Malone", "Sunflower", Duration.ZERO, 3, "myDirectory");
+			gson.toJson(song, fileWriter);
+			
+			fileWriter.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
