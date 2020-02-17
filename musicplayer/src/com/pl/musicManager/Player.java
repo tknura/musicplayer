4a4f1ed43c7ff6e42b3acf133bb3b17e437ef1a8
Tomasz Knura <tknura@gmail.com>
2020-02-17 21:22:29 +0100

    Added song time proccesbar, some styling and changes in player

--- musicplayer/src/com/pl/musicManager/Player.java
+++ musicplayer/src/com/pl/musicManager/Player.java
@@ -2,35 +2,55 @@ package com.pl.musicManager;
 
 import java.io.File;
 
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-
+import javafx.util.Duration;
 
 public class Player {
 	
-	private MediaPlayer mediaPlayer;
-	private Queue songsQueue;
-	private Song currentPlayingSong;
+	private static MediaPlayer mediaPlayer;
+	private  static Queue songsQueue;
+	private static Duration currentDuration;
+	private static Song currentPlayingSong;
 	
-	public Queue getSongsQueue() {
+	public static Queue getSongsQueue() {
 		return songsQueue;
 	}
 
-	public void addToQueue(Song song) {
+	public static void addToQueue(Song song) {
 		songsQueue.add(song);
 	}
 	
-	public void Play(Song song) {
+	public static Song getCurrentPlayingSong() {
+		return currentPlayingSong;
+	}
+
+	public static void setCurrentPlayingSong(Song currentPlayingSong) {
+		Player.currentPlayingSong = currentPlayingSong;
+	}
+
+	public static void Load(Song song) {
 		Media currentSongMedia = new Media(new File(song.getDirectory()).toURI().toString());
 		currentPlayingSong = song;
-		currentPlayingSong.setPlaying(false);
 		mediaPlayer = new MediaPlayer(currentSongMedia);
-		song.setPlaying(true);
-		mediaPlayer.play();
 	}
 	
-	public void Pause() {
+	public static void Load() {
+		Load(currentPlayingSong);
+	}
+	
+	public static void Resume() {
+		mediaPlayer.play();	
+		currentPlayingSong.setPlaying(true);
+	}
+	
+	public static void Pause() {
 		mediaPlayer.pause();
+		currentPlayingSong.setPlaying(false);
+	}
+	
+	public static ReadOnlyObjectProperty<Duration> currentTimeProperty(){
+		return mediaPlayer.currentTimeProperty();
 	}
 }
--- musicplayer/src/view/PlayerController.java
+++ musicplayer/src/view/PlayerController.java
@@ -1,9 +1,19 @@
 package view;
 
+import java.time.Duration;
+
+import com.pl.musicManager.Player;
+import com.pl.musicManager.Song;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
 import javafx.scene.control.ToggleButton;
 
 public class PlayerController {
@@ -14,7 +24,7 @@ public class PlayerController {
 
 	@FXML private Button nextButton;
 
-	@FXML private Button playPauseButton;
+	@FXML private ToggleButton playPauseButton;
 	
 	@FXML private Button prevButton;
 	
@@ -25,4 +35,57 @@ public class PlayerController {
 	@FXML private ToggleButton volumeButton;
 
 	@FXML private ToggleButton repeatButton;
+	
+	@FXML private Label songDuration;
+	
+	@FXML private Slider timeSlider;
+	
+	@FXML private ProgressBar timeProgressBar;
+	
+	@FXML public void initialize() {
+		Song song = new Song(1, "E:/Repositories/musicplayer/test.mp3", "schafter", "schafter", "schafter", Duration.ofSeconds(1) , 0);
+		Player.setCurrentPlayingSong(song);
+		Player.Load(song);
+		
+		timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+            	timeProgressBar.setProgress(new_val.doubleValue()/100);
+            }
+        });
+//	    Player.currentTimeProperty().addListener((ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) -> {
+//	    	timeSlider.setValue(newValue.toSeconds());
+//	    });
+	}
+	
+	@FXML private void handleNextButton(ActionEvent event) {
+		
+	}
+	
+	@FXML private void handlePrevButton(ActionEvent event) {
+		
+	}
+	
+	@FXML private void handlePlayPauseButton(ActionEvent event) {
+		if(Player.getCurrentPlayingSong() != null) {
+			if(!playPauseButton.isSelected()) {
+				handlePause();
+			}
+			else {
+				handlePlay();
+			}
+		}
+	}
+	
+	private void handlePlay() {
+		Player.Resume();
+		playIcon.setOpacity(0);
+		pauseIcon.setOpacity(1);
+	}
+	
+	private void handlePause() {
+		Player.Pause();
+		playIcon.setOpacity(1);
+		pauseIcon.setOpacity(0);
+	}
+
 }
